f3fccdb8585151f7556ffb06cea1c1c08bff7407
MariaEduardaOrty <142507594+MariaEduardaOrty@users.noreply.github.com>
2025-09-10 20:54:18 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const caixaPrincipal=document.querySelector(".caixa-principal");
+const caixaPerguntas=document.querySelector(".caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.querySelector(".caixa-resultado");
+const textoResultado=document.querySelector(".texto-resultado");
+const perguntas=[
+    {
+        enunciado:"assim que saiu do colégio você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios. Qual é o seu primeiro pensamento?",
+        alternativas: [
+            {
+                texto:"Isso é assustador,"
+            }
+        ]
+    }
+]
+
